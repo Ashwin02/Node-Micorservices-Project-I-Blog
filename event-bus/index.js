88288e58820b6b8d9e1e7d6b85e47d386e5b9e38
Ashwin Patil <ashwin.s.patil10@gmail.com>
2020-10-13 13:58:18 -0400

    Implemented event sync for missed events

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,8 +5,13 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+/** Event store */
+const events = [];
+
 app.post('/events', (req, res) =>{
     const event = req.body;
+
+    events.push(event);
 
     axios.post('http://localhost:4000/events', event);
     axios.post('http://localhost:4001/events', event);
@@ -15,6 +20,10 @@ app.post('/events', (req, res) =>{
 
     res.send({status: 'OK'});
 });
+
+app.get('/events', (req, res) =>{
+    return res.send(events);
+})
 
 app.listen(4005, () =>{
     console.log("Server listning on port 4005");
